cc258a34ee8c3a7fff4c2d2fe9b0736968530720
wwwouter <wouter@wercker.com>
2013-07-26 18:12:47 +0200

    failed/passed messages

--- run.sh
+++ run.sh
@@ -3,11 +3,6 @@ if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_TOKEN" ]; then
   exit 1
 fi
 
-if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_MESSAGE" ]; then
-  error 'Please specify message property'
-  exit 1
-fi
-
 if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_SUBDOMAIN" ]; then
   error 'Please specify subdomain property'
   exit 1
@@ -16,6 +11,38 @@ fi
 if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_ROOM_ID" ]; then
   error 'Please specify room_id property'
   exit 1
+fi
+
+
+
+if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_FAILED_MESSAGE" ]; then
+  if [ ! -n "$DEPLOY" ]; then
+    export WERCKER_CAMPFIRE_NOTIFY_FAILED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: build of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY failed."
+  else
+    export WERCKER_CAMPFIRE_NOTIFY_FAILED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
+  fi
+fi
+
+if [ ! -n "$WERCKER_CAMPFIRE_NOTIFY_PASSED_MESSAGE" ]; then
+  if [ ! -n "$DEPLOY" ]; then
+    export WERCKER_CAMPFIRE_NOTIFY_PASSED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: build of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY passed."
+  else
+    export WERCKER_CAMPFIRE_NOTIFY_PASSED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY passed."
+  fi
+fi
+
+if [ "$WERCKER_RESULT" = "passed" ]; then
+  export WERCKER_CAMPFIRE_NOTIFY_MESSAGE="$WERCKER_CAMPFIRE_NOTIFY_PASSED_MESSAGE"
+else
+  export WERCKER_CAMPFIRE_NOTIFY_MESSAGE="$WERCKER_CAMPFIRE_NOTIFY_FAILED_MESSAGE"
+fi
+
+
+if [ "$WERCKER_CAMPFIRE_NOTIFY_ON" = "failed" ]; then
+  if [ "$WERCKER_RESULT" = "passed" ]; then
+    echo "Skipping.."
+    return 0
+  fi
 fi
 
 
